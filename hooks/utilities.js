58d31e2612328883f5243718bffb550c6b2b7a82
Juragan <wido16884@gmail.com>
2023-08-09 15:54:57 +0700

    update utilities.js

--- hooks/utilities.js
+++ hooks/utilities.js
@@ -89,13 +89,12 @@ function getPlatformConfigs(platform) {
 
 function getZipFile(folder, zipFileName) {
   try {
+    console.log("getZipFile Folder : " + folder);
     var files = getFilesFromPath(folder);
     for (var i = 0; i < files.length; i++) {
-      if (files[i].endsWith(constants.zipExtension)) {
-        var fileName = path.basename(files[i], constants.zipExtension);
-        if (fileName === zipFileName) {
+      console.log("getZipFile file : " + files[i]);
+      if (files[i].startsWith(zipFileName) && files[i].endsWith(constants.zipExtension)) {
           return path.join(folder, files[i]);
-        }
       }
     }
   } catch (e) {
